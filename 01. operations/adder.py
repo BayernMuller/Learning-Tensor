ac203f2d1b858bc54a65fe28c47bb76bce7d43bc
BayernMuller <cjjunk29@gmail.com>
2021-01-15 19:56:06 +0900

    [Modify] tensorflow v2 to v1

--- 01. operations/adder.py
+++ 01. operations/adder.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 node1 = tf.constant(3.0)
 node2 = tf.constant(4.0)
 
-@tf.function
-def forward():
-    return node1 + node2
+node3 = tf.add(node1, node2)
 
-print(forward())
+sess = tf.Session()
+print(sess.run(node3))
