2643ba594503cea031352d05134a36f5d3389ba0
BayernMuller <cjjunk29@gmail.com>
2021-03-16 11:07:46 +0900

    [Add] basic_linear_regression.py

---
+++ 01_Linear_Regression/Readme.md
@@ -0,0 +1,9 @@
+# 01_Linear_Regression
+
+x, y 좌표를 받아 1차 함수 Wx + b의 W, b를 추정한다.
+
+![](images/linear_regression.png)
+
+![](images/mse.png)
+
+추정한 y = Wx + b 의 값과 데이터의 차의 제곱의 평균이 적어지는 방향으로 학습한다. 
---
+++ 01_Linear_Regression/basic_linear_regression.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+W = tf.Variable(tf.random_normal([1])) # shape is 1 * 1
+b = tf.Variable(tf.random_normal([1])) # shape is 1 * 1
+x = tf.placeholder(tf.float32)  # domain
+# placeholder make you can put value later
+
+lenear_model = W * x + b
+
+y = tf.placeholder(tf.float32) # range
+
+loss = tf.reduce_mean(tf.square(lenear_model - y)) # MSE loss function
+
+optimizer = tf.train.GradientDescentOptimizer(0.01) # set running rate
+# I don't know how running rate works yet.
+train_step = optimizer.minimize(loss) 
+# train to loss function's result keeps lower
+
+x_train = list(range(0, 10)) # real input         [0, 1, 2, ..., 9]
+y_train = list(range(0, 30, 3)) # real output     [0, 3, 6, ..., 27]
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for i in range(1000):
+    sess.run(train_step, feed_dict={x: x_train, y: y_train})
+    # train with real x and y
+
+x_test = [4, 8, 10, 17]
+# test x values
+
+print(sess.run(lenear_model, feed_dict={x: x_test}))
+# print W * x_test + b
+
+sess.close()
